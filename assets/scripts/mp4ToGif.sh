150c586557a1c2881d8999eded6ed9e420e39a2f
adrianensis <adrianensis@gmail.com>
2022-11-10 00:50:57 +0100

    first intro post + gifs

---
+++ assets/scripts/mp4ToGif.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+for f in *.mp4
+do
+    echo ">>>>>>>>>>>>>>>>>>>>> Converting $f"
+    ffmpeg -y -hide_banner -loglevel error -ss 00:00:05 -to 00:00:25 -i "$f" -filter_complex "fps=15,scale=600:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=64[p];[s1][p]paletteuse=dither=bayer" "$f.gif" &
+done
+
+wait
