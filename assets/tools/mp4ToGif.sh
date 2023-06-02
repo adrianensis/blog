e82dca700b887756c6e9bfeb1e4fef8b05e54caf
adrianensis <adrianensis@gmail.com>
2023-06-02 10:34:54 +0200

    update

--- assets/tools/mp4ToGif.sh
+++ assets/tools/mp4ToGif.sh
@@ -3,7 +3,8 @@
 for f in *.mp4
 do
     echo ">>>>>>>>>>>>>>>>>>>>> Converting $f"
-    ffmpeg -y -hide_banner -loglevel error -ss 00:00:05 -to 00:00:25 -i "$f" -filter_complex "fps=15,scale=600:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=64[p];[s1][p]paletteuse=dither=bayer" "$f.gif" &
+    ffmpeg -y -hide_banner -loglevel error -ss 00:00:05 -to 00:00:25 -i "$f" -filter_complex "fps=15,scale=900:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=64[p];[s1][p]paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" "$f.gif" &
+    #ffmpeg -y -hide_banner -loglevel error -ss 00:00:05 -to 00:00:25 -i "$f" -filter_complex "fps=15,scale=900:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=64[p];[s1][p]paletteuse=dither=bayer" "$f.gif" &
 done
 
 wait
